6cf6a07750f88ec00be58812f8b50894dc2bcf9d
aleksandia <98323601+aleksandia@users.noreply.github.com>
2025-01-31 23:20:48 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 const config = {
   images: {
     remotePatterns: [
@@ -13,17 +11,9 @@ const config = {
     ignoreBuildErrors: process.env.VERCEL_ENV === 'production',
   },
   eslint: {
-    // Set this to false if you want production builds to abort if there's lint errors
+    /// Set this to false if you want production builds to abort if there's lint errors
     ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      // Hier wird der Alias 'components' auf den absoluten Pfad des components-Ordners gesetzt:
-      'components': path.resolve(__dirname, 'components'),
-    };
-    return config;
-  },
-};
+}
 
-module.exports = config;
+export default config
